18c154e7c615cb521533068814901e199b47a496
Knightmare001 <egiprayogi07@gmail.com>
2023-09-03 17:27:27 +0700

    menambahkan dark mode

--- script.js
+++ script.js
@@ -36,56 +36,128 @@ nama_member.forEach(function (member) {
 });
 
 const posScrollYFirst = document.documentElement.scrollTop;
+const icon = document.querySelector(".containerHeader i");
+const body = document.querySelector("body");
+let mode = "";
 
-window.addEventListener("scroll", function () {
-  // mendapatkan posisi scroll Y
-  let posScrollY = document.documentElement.scrollTop;
-
-  // mengubah logo dan hamburgerMenu menjadi hitam
-  if (posScrollY >= 820) {
-    logo.src = "./img/logo_black.png";
-    line.forEach(function (e) {
-      e.style.backgroundColor = "black";
-    });
-  }
-
-  // mengubah logo social media menjadi hitam
-  else if (posScrollY >= 130) {
-    socialMedia.forEach((e) => {
-      const temp = e.attributes.src.value.replace("_white.png", "_black.png");
-      e.attributes.src.value = temp;
-    });
-    logo.src = "./img/logo.png";
-    line.forEach(function (e) {
-      e.style.backgroundColor = "white";
-    });
-  }
-
-  // mengembalikan logo, hamburgerMenu dan logo social media menjadi seperti awal
-  else {
-    logo.src = "./img/logo.png";
-    line.forEach(function (e) {
-      e.style.backgroundColor = "white";
-    });
-    socialMedia.forEach((e) => {
-      const temp = e.attributes.src.value.replace("_black.png", "_white.png");
-      e.attributes.src.value = temp;
-    });
-  }
-});
-
-if (document.documentElement.scrollTop > 130) {
+const theme = localStorage.getItem("theme");
+if (theme === "dark") {
+  icon.className = "bi bi-brightness-high-fill";
+  icon.id = "sun";
+  document.querySelector("body").className = "dark_mode";
+} else {
+  icon.className = "bi bi-moon-stars-fill";
+  icon.id = "moon";
+  document.querySelector("body").className = "light_mode";
+}
+if (theme === "dark") {
+  console.log("work");
+  socialMedia.forEach((e) => {
+    const temp = e.attributes.src.value.replace("_black.png", "_white.png");
+    e.attributes.src.value = temp;
+  });
+} else if (
+  document.querySelector("body").className === "light_mode" &&
+  document.documentElement.scrollTop > 130
+) {
   socialMedia.forEach((e) => {
     const temp = e.attributes.src.value.replace("_white.png", "_black.png");
     e.attributes.src.value = temp;
   });
 }
 
-// window.addEventListener("scroll", function () {
-//   let posScrollY = document.documentElement.scrollTop;
-//   if (posScrollY >= 80) {
-//     socialMedia[3].src = "./img/tiktok_black.png";
-//   } else if (posScrollY >= 130) socialMedia[2].src = "./img/youtube_black.png";
-//   else if (posScrollY >= 170) socialMedia[1].src = "./img/twitter_black.png";
-//   else if (posScrollY >= 220) socialMedia[0].src = "./img/ig_black.png";
-// });
+window.addEventListener("click", (e) => {
+  if (e.target === icon) {
+    if (icon.id === "moon") {
+      e.target.className = "bi bi-brightness-high-fill";
+      e.target.id = "sun";
+      mode = "dark";
+      localStorage.setItem("theme", mode);
+      document.querySelector("body").className = "dark_mode";
+      changeNavDownColorToWhite();
+      changeNavUpColorToWhite();
+    } else {
+      e.target.className = "bi bi-moon-stars-fill";
+      e.target.id = "moon";
+      document.querySelector("body").className = "light_mode";
+      mode = "light";
+      localStorage.setItem("theme", mode);
+      changeNavDownColorToBlack();
+      changeNavUpColorToBlack();
+    }
+  }
+});
+
+changeColorOnScroll();
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+function changeNavUpColorToBlack() {
+  logo.src = "./img/logo_black.png";
+  icon.style.color = "black";
+  line.forEach(function (e) {
+    e.style.backgroundColor = "black";
+  });
+}
+
+function changeNavDownColorToWhite() {
+  socialMedia.forEach((e) => {
+    const temp = e.attributes.src.value.replace("_black.png", "_white.png");
+    e.attributes.src.value = temp;
+  });
+}
+
+function changeNavDownColorToBlack() {
+  socialMedia.forEach((e) => {
+    const temp = e.attributes.src.value.replace("_white.png", "_black.png");
+    e.attributes.src.value = temp;
+  });
+}
+
+function changeNavUpColorToWhite() {
+  logo.src = "./img/logo.png";
+  line.forEach(function (e) {
+    e.style.backgroundColor = "white";
+  });
+
+  icon.style.color = "white";
+}
+
+function changeColorOnScroll() {
+  window.addEventListener("scroll", function () {
+    if (document.querySelector("body").className === "dark_mode") {
+    } else {
+      let posScrollY = document.documentElement.scrollTop;
+
+      // mengubah logo dan hamburgerMenu menjadi hitam
+      if (posScrollY >= 820) {
+        changeNavUpColorToBlack();
+      }
+
+      // mengubah logo social media menjadi hitam
+      else if (posScrollY >= 130) {
+        changeNavUpColorToWhite();
+        changeNavDownColorToBlack();
+      }
+
+      // mengembalikan logo, hamburgerMenu dan logo social media menjadi seperti awal
+      else {
+        changeNavDownColorToWhite();
+        changeNavUpColorToWhite();
+      }
+    }
+  });
+}
